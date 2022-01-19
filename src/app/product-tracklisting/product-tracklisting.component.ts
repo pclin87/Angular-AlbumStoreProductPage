b636b3a4b80632b6a33cbd7dfd70eca62626ebdd
pclin87 <73913675+pclin87@users.noreply.github.com>
2022-01-19 13:19:05 -1000

    4

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-product-tracklisting',
+    templateUrl: './product-tracklisting.html',
+    styleUrls: './product-tracklisting.css'
+})
+
+export class ProductTracklisting {
+
+    constructor(){}
+}
