3b8d226a9ae56b9336181280fb8821f309991ce8
pclin87 <73913675+pclin87@users.noreply.github.com>
2022-01-19 10:42:30 -1000

    module 1

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,19 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-product-description',
-    template: `<div class="row description">
-    <div class="col-sm-2">
-      <img src="assets/opacity-zero.jpg" alt="product image" />
-    </div>
-    <div class="col-sm-6">
-      <p class="album-name">Opacity Zero</p>
-      <p class="band-name">The Prependers</p>
-      <p class="album-release-date">September 25, 2014</p>
-    </div>
-  </div>`
+    templateUrl: './product-description.component.html',
+    styleUrls: ['./product-description.component.css']
 })
 
-export class ProductDescriptionComponent {
+export class ProductDescriptionComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit(){}
 
 }
