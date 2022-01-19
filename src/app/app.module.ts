c0b2b41fe2e39dfaac2e7682911a9051d36eb449
pclin87 <73913675+pclin87@users.noreply.github.com>
2022-01-19 13:19:38 -1000

    5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,17 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductTracklisting } from './product-tracklisting/product-tracklisting.component';
 
 import { ProductService } from './product.service'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    ProductTracklisting
   ],
   imports: [
     BrowserModule,
