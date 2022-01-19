a9f57b39e3e9a510b0834af771a55e6f66e3b7ee
pclin87 <73913675+pclin87@users.noreply.github.com>
2022-01-19 12:14:41 -1000

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-import { ProductService } from './product.service';
+
+import { ProductService } from './product.service'
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { ProductService } from './product.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
+    HttpModule,
 
   ],
   providers: [ProductService],
